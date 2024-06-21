0bde5ec6a6f76d3a07a5b3e54b37b42b0cf2cfb6
SITEMAX-design <sitemaxgold@gmail.com>
2024-06-21 13:23:21 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].classList.add("active");
+    setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
